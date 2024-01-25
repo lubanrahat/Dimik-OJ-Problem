d238ab9ce30e25fa63bf6ca5058752db98ab63ef
Luban Rahat <lubanrahat@Lubans-Mac-mini.local>
2024-01-25 23:22:15 +0600

    Presentation error

--- Descending_Number.cpp
+++ Descending_Number.cpp
@@ -8,12 +8,10 @@ int main()
     cin.tie(0);
 
     for(int i = 1000; i >= 1; i--) {
-        
-        if(i % 5 == 0) cout<<"\n";
-        
         cout<<i<<" ";
+        if((i-1) % 5 == 0) cout<<"\n";
+        
     }
-    cout<<endl;
-
+    
     return 0;
 }
